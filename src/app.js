ce3d20e1f359d62f2b560c6afae9c53cf774f391
halilseker <g141210082@sakarya.edu.tr>
2023-01-11 19:14:03 +0000

    Initial commit frontend project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+var element = document.querySelector('.secondSpan');
+//var indicatorsNums = document.querySelector('.indicatorsNums');
+
+var query = window.matchMedia('(min-width: 778px)');
+
+function handleChange(query) {
+  if (query.matches) {
+    element.classList.remove('justify-content-center');
+    element.classList.add('justify-content-start');
+    // indicatorsNums.classList.remove('col-md-2');
+    // indicatorsNums.classList.add('col-2');
+  } else {
+    element.classList.remove('justify-content-start');
+    element.classList.add('justify-content-center');
+
+    // indicatorsNums.classList.remove('col-2');
+    // indicatorsNums.classList.add('col-md-2');
+  }
+}
+query.addEventListener('change', handleChange);
+handleChange(query);
+
+indicatorsNums;
